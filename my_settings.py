4126d68019c6f53babe49bb462e7ce2ec2f6aaab
damdream <possible_11@naver.com>
2021-07-18 23:24:32 +0900

    프로젝트 셋팅

---
+++ my_settings.py
@@ -0,0 +1,13 @@
+DATABASES = {
+    'default' : {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'westarbucks ',
+        'USER': 'dodam',
+        'PASSWORD': '0203',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+
+
+SECRET_KEY = 'django-insecure-=-u04t7qnhtf&ie9jra#!+5c049d=fhs&il*x598g4z!j$0hq$'
